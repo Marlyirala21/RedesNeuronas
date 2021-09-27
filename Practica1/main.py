8c5745a030f79b4c152dd7a6da5f58af25aa3186
Marly <marlyirala21@github.com>
2021-09-27 14:04:59 +0200

    Ya muestra los errores de entrenamiento (+ una grafica cutre para ver la evolucion) He cambiado lo del bucle while y he puesto que ejecute 7 ciclos (por ejemplo)

--- Practica1/main.py
+++ Practica1/main.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import random
-import matplotlip as plt
+import matplotlib.pyplot as plt
 
 # leemos y guardamos los datos
 df = pd.read_csv('dataset.csv', header=0)
@@ -22,7 +22,7 @@ validate.to_csv("validate.csv")
 test.to_csv("test.csv")
 
 class Adaline():
-    def __init__(self, wi, entradas , b, factAprendizaje, d, n, ciclos, errorAceptado):
+    def __init__(self, wi, entradas , b, factAprendizaje, d, n, ciclos):
         self.wi = wi
         self.entradas = entradas
         self.b = b
@@ -30,57 +30,58 @@ class Adaline():
         self.d = d
         self.n = n
         self.ciclos = ciclos
-        self.errorAceptado = errorAceptado
         self.y = 0 #salida de la red
 
     #funcion de entrenamiento
     def entrenamiento(self):
-        e = 1
-        eAnterior = 0
+        e = 0
         eFila = 0 #error actual de cada patrón/fila
         mse = 0 #error cuadrático medio
         eRed= []
+        eAnterior = 0
         eAcumulado = 0 #error acumulado de cada patrón
 
-        while (np.abs(e) > self.errorAceptado):
+        while (self.ciclos < 7):
             eAnterior = mse
             for i in range (self.n):
                 #calculamos la salida para cada patron/fila
-                self.y = np.dot(entradas[i], wi) + b
+                self.y = np.dot(self.entradas[i], self.wi) + self.b #************PREGUNTAR SI ESTA BIEN********************
                 #ajuste de pesos y umbral/bias
-                self.eFila = (self.d[i] - self.y) #error actual de cada fila
-                self.wi = self.wi + (self.factAprendizaje * eFila * entradas[i,:] )
+                eFila = (self.d[i] - self.y) #error actual de cada fila
+                self.wi = self.wi + (self.factAprendizaje * eFila * self.entradas[i,:] )
                 self.b = self.factAprendizaje * eFila
                 # calculamos el error acumulado para posteriormente calcular el mse
-                self.eAcumulado = eAcumulado + pow(eFila,2)
+                eAcumulado = eAcumulado + pow(eFila, 2)
 
-            #calculamos el error cuadrático medio
-            mse = ((1/self.n) * (self.eAcumulado))
-            e = mse - eAnterior
+            #calculamos el error cuadrático medio de cada ciclo
+            mse = ((1/self.n) * (eAcumulado))
+            e = (mse - eAnterior)
             eRed.append(np.abs(e))
             self.ciclos += 1
+            print("este es el ciclo " + str(self.ciclos) + " " + " y tiene un error de: " + str(e))
         return self.wi, self.ciclos, eRed
 
 """DUDA: es aconsejable mostrar en pantalla el error medio a lo largo de los ciclos de aprendizaje.
-Se refiere a ir mostrando el mse de cada ciclo como esta ahora o hay que calcular el error de cada ciclo, 
-es decir, habria que ir añadiendo -> e = mse - mseAnterior (siendo msePrevio = mse al principio del ciclo)
+Se refiere a ir mostrando el mse de cada ciclo o hay que calcular el error del mse de cada ciclo, 
+es decir, hay que ir restando el mse actual al anterior y devolver la diferencia
+
+DUDA 2: condicion de parada, como se haria lo de los ciclos
+DUDA 3: todo lo del error de validacion
 
 """
-
-
-
 
 if __name__ == '__main__':
     #convertimos los datos en una matriz
     data = train.to_numpy()
     #variables de entrada de los datos de entrenamiento
     entradas = data[:, 0:8]
-    d = data[:, 8]#variable deseada
+    #variable deseada
+    d = data[:, 8]
     #vector pesos w
     wi = np.random.rand(8)
     #w ajustado
     w = []
-    # factor de aprendizaje
+    #factor de aprendizaje
     factAprendizaje = 0.5
     #numero de muestras
     n = len(d) #721
@@ -88,13 +89,20 @@ if __name__ == '__main__':
     b = random.uniform(0, 1)
     #ciclos
     ciclos = 0
-    #error aceptado
-    errorAceptado = 0.001
     #inicializamos la red
-    red = Adaline(wi, entradas , b, factAprendizaje, d, n, ciclos, errorAceptado)
+    red = Adaline(wi, entradas , b, factAprendizaje, d, n, ciclos)
     wAjustado, ciclos, error = red.entrenamiento()
+    #grafica
+    fig, ax = plt.subplots()
+    ciclos = np.arange(ciclos)
+    error = {'error': error}
+    ax.plot(ciclos, error['error'], marker = 'o')
+    ax.set_title('Evolución del error', loc="left",
+                 fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
+    plt.show()
 
-    print(ciclos)
+
+
 
 
 
@@ -107,4 +115,4 @@ if __name__ == '__main__':
 
     
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
